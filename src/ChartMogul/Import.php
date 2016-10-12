d533c0d47e2d820ae48059fd0945a30604638e44
Keith Palmer Jr <kpalmer@Keiths-MacBook-Pro-2.local>
2016-10-12 01:20:07 -0400

    Initial implementation.

---
+++ src/ChartMogul/Import.php
@@ -0,0 +1,316 @@
+<?php 
+
+namespace ChartMogul;
+ 
+class Import 
+{
+	const BASE = 'https://api.chartmogul.com/v1/import/';
+
+	protected $_access_token;
+	protected $_secret_key;
+
+	protected $_data_source_uuid;
+
+	protected $_last_error;
+
+	public function __construct($access_token, $secret_key, $data_source_uuid = null)
+	{
+		$this->_access_token = $access_token;
+		$this->_secret_key = $secret_key;
+
+		$this->_data_source_uuid = $data_source_uuid;
+	}
+
+	public function ping()
+	{
+		
+	}
+
+	protected function _request($endpoint, $payload = null)
+	{
+		$Http = new Util\Http(self::BASE . $endpoint);
+
+		$Http->setRawBody(json_encode($payload));
+
+		$Http->setAuth($this->_access_token, $this->_secret_key);
+		
+		$Http->verifyPeer(false);
+		$Http->verifyHost(false);
+
+		$Http->setHeaders(array(
+			'Content-Type: application/json', 
+			));
+
+		if ($payload)
+		{
+			$out = $Http->POST();
+		}
+		else
+		{
+			$out = $Http->GET();
+		}
+
+		error_log('last response: ' . $Http->lastResponse());
+		//error_log('last info: ' . print_r($Http->lastInfo(), true));
+
+		return json_decode($out);
+	}
+
+	public function setDataSource($uuid)
+	{
+		$this->_data_source_uuid = $uuid;
+	}
+
+	public function listDataSources()
+	{
+		/*
+		https://api.chartmogul.com/v1/import/data_sources
+		 */
+		
+		$out = $this->_request('data_sources');
+
+		return $out->data_sources;
+	}
+
+	public function dataSource($name)
+	{
+		/*
+		# 1. Create a Data Source
+curl -X POST "https://api.chartmogul.com/v1/import/data_sources" \
+	 -u YOUR_ACCOUNT_TOKEN:YOUR_SECRET_KEY \
+	 -H "Content-Type: application/json" \
+	 -d '{ 
+		  "name": "In-house billing"
+		 }'
+		 */
+		
+		$out = $this->_request('data_sources', array(
+			'name' => $name, 
+			));
+
+		return $this->_handleErrors($out);
+	}
+
+	protected function _handleErrors($out)
+	{
+		if (!empty($out->errors))
+		{
+			$this->_last_error = $out->errors;
+
+			return false;
+		}
+
+		return $out;
+	}
+
+	public function lastErrors()
+	{
+		return $this->_last_error;
+	}
+
+	public function customer($customer)
+	{
+		/*
+		# 2. Create Customers
+curl -X POST "https://api.chartmogul.com/v1/import/customers" \
+	 -u YOUR_ACCOUNT_TOKEN:YOUR_SECRET_KEY \
+	 -H "Content-Type: application/json" \
+	 -d '{
+		  "data_source_uuid": "ds_fef05d54-47b4-431b-aed2-eb6b9e545430",
+		  "external_id": "cus_0001",
+		  "name": "Adam Smith",
+		  "email": "adam@smith.com",
+		  "country": "US",
+		  "city": "New York"
+		 }'
+		 */
+		
+		if (empty($customer['data_source_uuid']) and 
+			$this->_data_source_uuid)
+		{
+			$customer['data_source_uuid'] = $this->_data_source_uuid;
+		}
+
+		$out = $this->_request('customers', $customer);
+
+		return $this->_handleErrors($out);
+	}
+
+	public function plan($plan)
+	{
+		/*
+		# 3. Create Subscription Plans
+curl -X POST "https://api.chartmogul.com/v1/import/plans" \
+	 -u YOUR_ACCOUNT_TOKEN:YOUR_SECRET_KEY \
+	 -H "Content-Type: application/json" \
+	 -d '{ 
+		   "data_source_uuid": "ds_fef05d54-47b4-431b-aed2-eb6b9e545430",
+		   "name": "Bronze Plan",
+		   "interval_count": 1,
+		   "interval_unit": "month",
+		   "external_id": "plan_0001"
+		 }'
+		 */
+		
+		if (empty($plan['data_source_uuid']) and 
+			$this->_data_source_uuid)
+		{
+			$plan['data_source_uuid'] = $this->_data_source_uuid;
+		}
+
+		$out = $this->_request('plans', $plan);
+
+		return $this->_handleErrors($out);
+	}
+
+	public function invoices($invoices)
+	{
+		$first = $invoices[0];
+
+		$out = $this->_request('customers/' . $first['customer_uuid'] . '/invoices', array( 'invoices' => $invoices ));
+
+		return $this->_handleErrors($out);
+	}
+
+	public function invoice($invoice)
+	{
+		$resp = $this->invoices(array( $invoice ));
+
+		if ($this->_handleErrors($resp))
+		{
+			return $resp->invoices[0];
+		}
+
+		return false;
+
+		/*
+		# 4. Send Invoices
+curl -X POST "https://api.chartmogul.com/v1/import/customers/cus_f466e33d-ff2b-4a11-8f85-417eb02157a7/invoices" \
+	 -u YOUR_ACCOUNT_TOKEN:YOUR_SECRET_KEY \
+	 -H "Content-Type: application/json" \
+	 -d '{
+	 "invoices":[
+	   {
+		  "external_id": "INV0001",
+		  "date": "2015-11-01 00:00:00",
+		  "currency": "USD",
+		  "due_date": "2015-11-15 00:00:00",
+		  "line_items": [
+			{
+			  "type": "subscription",
+			  "subscription_external_id": "sub_0001",
+			  "plan_uuid":"pl_eed05d54-75b4-431b-adb2-eb6b9e543206",
+			  "service_period_start": "2015-11-01 00:00:00",
+			  "service_period_end": "2015-12-01 00:00:00",
+			  "amount_in_cents": 5000,
+			  "quantity": 1,
+			  "discount_code": "PSO86",
+			  "discount_amount_in_cents": 1000,
+			  "tax_amount_in_cents": 900
+			}
+		  ],
+		  "transactions": [
+			{
+			  "date": "2015-11-05 00:14:23",
+			  "type": "payment",
+			  "result": "successful"
+			}
+		  ]   
+	   },
+	   {
+		  "external_id": "INV0002",
+		  "date": "2015-12-01 00:00:00",
+		  "currency": "USD",
+		  "due_date": "2015-12-15 00:00:00",
+		  "line_items": [
+			{
+			  "type": "subscription",
+			  "subscription_external_id": "sub_0001",
+			  "plan_uuid":"pl_eed05d54-75b4-431b-adb2-eb6b9e543206",
+			  "service_period_start": "2015-12-01 00:00:00",
+			  "service_period_end": "2016-01-01 00:00:00",
+			  "amount_in_cents": 5000,
+			  "quantity": 1,
+			  "discount_code": "PSO86",
+			  "discount_amount_in_cents": 1000,
+			  "tax_amount_in_cents": 900
+			}
+		  ],
+		  "transactions": [
+			{
+			  "date": "2015-12-05 07:54:02",
+			  "type": "payment",
+			  "result": "successful"
+			}
+		  ]   
+	   }
+	 ]
+	 }'
+		 */
+		
+		// PRORATED
+		/*
+		# Example of a prorated charge for an additional subscription
+curl -X POST "https://api.chartmogul.com/v1/import/customers/cus_f466e33d-ff2b-4a11-8f85-417eb02157a7/invoices" \
+	 -u YOUR_ACCOUNT_TOKEN:YOUR_SECRET_KEY \
+	 -H "Content-Type: application/json" \
+	 -d '{
+	 "invoices":[
+	   {
+		  "external_id": "INV0004",
+		  "date": "2016-03-16 00:00:00",
+		  "currency": "USD",
+		  "line_items": [
+			{
+			  "type": "subscription",
+			  "subscription_external_id": "sub_0001",
+			  "plan_uuid":"pl_eed05d54-75b4-431b-adb2-eb6b9e543206",
+			  "prorated": true,
+			  "service_period_start": "2016-03-16 12:00:00",
+			  "service_period_end": "2016-04-01 00:00:00",
+			  "amount_in_cents": 2500,
+			  "quantity": 1
+			}
+		  ],
+		  "transactions": [
+			{
+			  "date": "2016-03-17 10:00:00",
+			  "type": "payment",
+			  "result": "successful"
+			}
+		  ]   
+		}
+	   ]
+}'
+		 */
+	}
+
+	public function cancel()
+	{
+		/*
+		# When a Subscription is cancelled
+curl -X PATCH "https://api.chartmogul.com/v1/import/subscriptions/sub_e6bc5407-e258-4de0-bb43-61faaf062035" \
+	 -u YOUR_ACCOUNT_TOKEN:YOUR_SECRET_KEY \
+	 -H "Content-Type: application/json" \
+	 -d '{ 
+		  "cancelled_at": "2016-01-15 00:00:00"
+		}'
+		 */
+	}
+
+	public function transaction()
+	{
+		/*
+		curl -X POST "https://api.chartmogul.com/v1/import/invoices/inv_565c73b2-85b9-49c9-a25e-2b7df6a677c9/transactions" \
+	 -u YOUR_ACCOUNT_TOKEN:YOUR_SECRET_KEY \
+	 -H "Content-Type: application/json"
+	 -d '{
+			"type": "refund",
+			"date": "2015-12-25 18:10:00",
+			"result": "successful"
+		 }'
+		 */
+	}
+
+
+}
---
+++ src/ChartMogul/Util/Http.php
@@ -0,0 +1,474 @@
+<?php
+
+namespace ChartMogul\Util;
+
+class Http
+{
+	const HTTP_400 = 400;
+	const HTTP_401 = 401;
+	const HTTP_500 = 500;
+
+	protected $_url;
+	
+	protected $_request_headers;
+	
+	protected $_body;
+	
+	protected $_post;
+	
+	protected $_get;
+	
+	protected $_last_response;
+	
+	protected $_last_request;
+	
+	protected $_last_duration;
+
+	protected $_last_info;
+	
+	protected $_errnum;
+	
+	protected $_errmsg;
+	
+	protected $_verify_peer;
+	
+	protected $_verify_host;
+	
+	protected $_certificate;
+	
+	protected $_log;
+	
+	protected $_debug;
+	
+	protected $_return_headers;
+
+	protected $_username;
+	protected $_password;
+	
+	/**
+	 * Create a new object to make HTTP requests to remote URLs
+	 * 
+	 * @param string $url		The URL to make an HTTP request to
+	 */
+	public function __construct($url = null)
+	{
+		$this->_url = $url;
+		
+		$this->_verify_peer = true;
+		$this->_verify_host = true;
+		
+		$this->_log = '';
+		
+		$this->_debug = false;
+		$this->_test = false;
+		
+		$this->_request_headers = array();
+		$this->_return_headers = false;
+		
+		$this->_last_request = null;
+		$this->_last_response = null;
+		$this->_last_duration = 0.0;
+	}
+	
+	/**
+	 * Set the URL
+	 * 
+	 * @param string $url
+	 * @return void
+	 */
+	public function setURL($url)
+	{
+		$this->_url = $url;
+	}
+	
+	/**
+	 * Get the URL 
+	 * 
+	 * @return string
+	 */
+	public function getURL()
+	{
+		// @TODO Support for query string args from ->setGETValues()
+		return $this->_url;
+	}
+	
+	public function verifyPeer($yes_or_no)
+	{
+		$this->_verify_peer = (boolean) $yes_or_no;
+	}
+	
+	public function verifyHost($yes_or_no)
+	{
+		$this->_verify_host = (boolean) $yes_or_no;
+	}
+	
+	public function setHeaders($arr)
+	{
+		foreach ($arr as $key => $value)
+		{
+			if (is_numeric($key) and 
+				false !== ($pos = strpos($value, ':')))
+			{
+				// 0 => "Header: value" format
+				
+				$key = substr($value, 0, $pos);
+				$value = ltrim(substr($value, $pos + 1));
+			}
+			
+			// "Header" => "value" format
+				
+			$this->setHeader($key, $value);
+		}
+	}
+	
+	/**
+	 * Tell whether or not to return the HTTP response headers 
+	 * 
+	 * @param boolean $return
+	 * @return void
+	 */
+	public function returnHeaders($return)
+	{
+		$this->_return_headers = (boolean) $return;
+	}
+	
+	public function setHeader($key, $value)
+	{
+		$this->_request_headers[$key] = $value;
+	}
+	
+	public function getHeaders($as_combined_array = false)
+	{
+		if ($as_combined_array)
+		{
+			$list = array();
+			foreach ($this->_request_headers as $key => $value)
+			{
+				$list[] = $key . ': ' . $value;
+			}
+			
+			return $list;
+		}
+		
+		return $this->_request_headers;
+	}
+	
+	public function getHeader($key)
+	{
+		if (isset($this->_request_headers[$key]))
+		{
+			return $this->_request_headers[$key];
+		}
+		
+		return null;
+	}
+	
+	public function setRawBody($str)
+	{
+		$this->_body = $str;
+	}
+	
+	public function setPOSTValues($arr)
+	{
+		$this->_post = $arr;
+	}
+	
+	public function setGETValues($arr)
+	{
+		$this->_get = $arr;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @return string
+	 */
+	public function getRawBody()
+	{
+		if ($this->_body)
+		{
+			return $this->_body;
+		}
+		else if (count($this->_post))
+		{
+			return http_build_query($this->_post);
+		}
+		
+		return '';
+	}
+
+	public function setAuth($user, $pass)
+	{
+		$this->_username = $user;
+		$this->_password = $pass;
+	}
+	
+	public function GET()
+	{
+		return $this->_request('GET');
+	}
+	
+	public function POST()
+	{
+		return $this->_request('POST');
+	}
+	
+	public function HEAD()
+	{
+		// @todo 
+	}
+	
+	public function useDebugMode($yes_or_no)
+	{
+		$prev = $this->_debug;
+		$this->_debug = (boolean) $yes_or_no;
+		
+		return $prev;
+	}
+
+	/**
+	 * Get the error number of the last error that occured
+	 * 
+	 * @return integer
+	 */
+	public function errorNumber()
+	{
+		return $this->_errnum;
+	}
+	
+	/**
+	 * Get the error message of the last error that occured
+	 * 
+	 * @return string
+	 */
+	public function errorMessage()
+	{
+		return $this->_errmsg;
+	}
+	
+	/**
+	 * Get the last raw XML response that was received
+	 * 
+	 * @return string
+	 */
+	public function lastResponse()
+	{
+		return $this->_last_response;
+	}
+	
+	/**
+	 * Get the last raw XML request that was sent
+	 *
+	 * @return string
+	 */
+	public function lastRequest()
+	{
+		return $this->_last_request;
+	}
+
+	/**
+	 * 
+	 *
+	 */
+	public function lastDuration()
+	{
+		return $this->_last_duration;
+	}
+
+	public function lastInfo()
+	{
+		return $this->_last_info;
+	}
+	
+	/**
+	 * Set an error message
+	 * 
+	 * @param integer $errnum	The error number/code
+	 * @param string $errmsg	The text error message
+	 * @return void
+	 */
+	protected function _setError($errnum, $errmsg = '')
+	{
+		$this->_errnum = $errnum;
+		$this->_errmsg = $errmsg;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * 
+	 * @param string $message
+	 * @param integer $level
+	 * @return boolean
+	 */
+	protected function _log($message)
+	{
+		if ($this->_debug)
+		{
+			print($message . "\r\n");
+		}
+		
+		// 
+		$this->_log .= $message . "\r\n";
+		
+		return true;
+	}
+	
+	public function resetLog()
+	{
+		$this->_log = '';
+	}
+	
+	public function getLog()
+	{
+		return $this->_log;
+	}
+	
+	/**
+	 * Make an HTTP request 
+	 * 
+	 * @param string $method
+	 * @return string
+	 */
+	protected function _request($method)
+	{
+		$start = microtime(true);
+		
+		if (!function_exists('curl_init'))
+		{
+			die('You must have the PHP cURL extension (php.net/curl) enabled to use this.');
+		}
+
+		$this->_log('Using CURL to send request!');
+		$return = $this->_requestCurl($method, $errnum, $errmsg);
+		
+		if ($errnum)
+		{
+			$this->_setError($errnum, $errmsg);
+		}
+		
+		// Calculate and set how long the last HTTP request/response took to make
+		$this->_last_duration = microtime(true) - $start;
+		
+		return $return;
+	}
+
+	protected function _requestCurl($method, &$errnum, &$errmsg)
+	{
+		$url = $this->getURL();
+		$raw_body = $this->getRawBody();
+		
+		$headers = $this->getHeaders(true);
+		
+		$this->_log('Opening connection to: ' . $url);
+		
+		$params = array();
+
+		if ($method == 'POST')
+		{
+			$headers[] = 'Content-Length: ' . strlen($raw_body);
+			$params[CURLOPT_POST] = true;
+			$params[CURLOPT_POSTFIELDS] = $raw_body;
+		}
+
+		$query = '';
+		if (count($this->_get))
+		{
+			$query = '?' . http_build_query($this->_get);
+		}
+
+		if ($qs = parse_url($url, PHP_URL_QUERY) and 
+			false !== strpos($qs, ' '))
+		{
+			$url = str_replace($qs, str_replace(' ', '+', $qs), $url);
+		}
+
+		$params[CURLOPT_RETURNTRANSFER] = true;
+		$params[CURLOPT_URL] = $url . $query;
+		$params[CURLOPT_HTTPHEADER] = $headers;
+		$params[CURLOPT_ENCODING] = '';			// This makes it say it supports gzip *and* deflate
+		
+		$params[CURLOPT_VERBOSE] = $this->_debug;
+		
+		if ($this->_return_headers)
+		{
+			$params[CURLOPT_HEADER] = true;
+		}
+		
+		// Some Windows servers will fail with SSL errors unless we turn this off
+		if (!$this->_verify_peer)
+		{
+			$params[CURLOPT_SSL_VERIFYPEER] = false;
+		}
+		
+		if (!$this->_verify_host)
+		{
+			$params[CURLOPT_SSL_VERIFYHOST] = 0;
+		}
+
+		if ($this->_username and $this->_password)
+		{
+			$params[CURLOPT_USERPWD] = $this->_username . ':' . $this->_password;
+		}
+		
+		// Fudge the outgoing request because CURL won't give us it
+		$request = '';
+		
+		if ($method == 'POST')
+		{
+			$request .= 'POST ';
+		}
+		else
+		{
+			$request .= 'GET ';
+		}
+		$request .= $params[CURLOPT_URL] . ' HTTP/1.1' . "\r\n";
+		
+		foreach ($headers as $header)
+		{
+			$request .= $header . "\r\n";
+		}
+		$request .= "\r\n";
+		$request .= $this->getRawBody();
+		
+		$this->_log('CURL options: ' . print_r($params, true));
+		
+		$this->_last_request = $request;
+		$this->_log('HTTP request: ' . $request);	// Set as DEBUG so that no one accidentally logs all the credit card numbers...
+		
+		$ch = curl_init();
+		curl_setopt_array($ch, $params);
+		$response = curl_exec($ch);
+		
+		/*
+		print("\n\n\n" . '---------------------' . "\n");
+		print('[[request ' . $request . ']]' . "\n\n\n");
+		print('[[resonse ' . $response . ']]' . "\n\n\n\n\n");
+		
+		print_r($params);
+		print_r(curl_getinfo($ch));
+		print_r($headers);
+		print("\n" . '---------------------' . "\n\n\n\n");
+		*/
+
+		$this->_last_response = $response;
+		$this->_log('HTTP response: ' . substr($response, 0, 500) . '...');
+		
+		$this->_last_info = curl_getinfo($ch);
+
+		if (curl_errno($ch)) 
+		{
+			$errnum = curl_errno($ch);
+			$errmsg = curl_error($ch);
+
+			$this->_log('CURL error: ' . $errnum . ': ' . $errmsg);
+			
+			return false;
+		} 
+		
+		// Close the connection 
+		@curl_close($ch);
+		
+		return $response;
+	}
+}
